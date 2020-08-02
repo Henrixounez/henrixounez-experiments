f6cc736b4db249988046fe4aa643f1069ea17405
Henrixounez <henrix.ounez@yahoo.fr>
2020-08-02 22:14:18 +0200

    Google verification

--- components/layout.js
+++ components/layout.js
@@ -8,6 +8,7 @@ const Layout = ({children, title, showPage}) => (
     <Head>
       <title>{title} | Henrixounez</title>
       <meta name="Description" content={title}/>
+      <meta name="google-site-verification" content="Xe9o0Xjf5IsRK8YWvMyPyo8PdvqRNXTCJXHy1A9obPM" />
     </Head>
     <Header title={title} showPage={showPage}/>
     <main className={css(tw`py-20 flex flex-col justify-center items-center`)}>
