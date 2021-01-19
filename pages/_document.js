4ac819350d3ce7366b3523757a805252172b67e6
Henrixounez <henrix.ounez@yahoo.fr>
2021-01-19 21:44:27 +0100

    add: google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@
 // ./pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import xw from 'xwind';
+import { GA_TRACKING_ID } from '../lib/gtag';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -14,8 +15,27 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
-        <link rel="stylesheet" as="style" href="https://rsms.me/inter/inter.css" />
+        <Head>
+          {GA_TRACKING_ID !== undefined && (
+            <>
+              {/* Global Site Tag (gtag.js) - Google Analytics */}
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </>
+          )}
+          <link rel="stylesheet" as="style" href="https://rsms.me/inter/inter.css" />
+        </Head>
         <body css={xw`bg-white text-black dark:bg-gray-900 dark:text-white duration-500`}>
           <Main />
           <NextScript />
