131d444d7fd6775f03c5cc4d7548e45bcf1cc98b
Henrixounez <henrix.ounez@yahoo.fr>
2021-02-13 22:26:11 +0100

    fix: packages + tsconfig

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ module.exports = {
     images: {
         domains: ['spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com'],
     },
-    pageExtensions: [".tsx"]
+    pageExtensions: [".tsx", ".js"]
 }
