2c2f99a1993b75e58b1d004ad7bab3cf3fcfc847
Henrixounez <henrix.ounez@yahoo.fr>
2021-02-15 09:19:07 +0100

    feat: intra links to spotify and coding

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import Layout from '../components/layout'
-import BigTitle from '../components/bigtitle'
-import LinkList from '../components/linklist'
+import Layout from '../components/layout';
+import BigTitle from '../components/bigtitle';
+import LinkList from '../components/linklist';
 
 const pages = [
   {link: '/spacetracker', intra: true, name: 'Space Tracker', description: 'Space events calendar', icon: '🚀'},
+  {link: '/coding', intra: true, name: 'Collaborative Coding', description: 'Online coding experience', icon: '🧑‍💻'},
+  {link: '/spotify', intra: true, name: 'Spotify Cities', description: 'Find out where your music is most listened from', icon: '🎶'},
   {link: '/geometry', intra: true, name: 'Geometry', description: 'Geometry visualisations', icon: '📐'},
   {link: '/workshops', intra: true, name: 'Workshops', description: 'Little coding workshops', icon: '💻'},
   // {link: '/avorion_goods', intra: true, name: 'Avorion Goods', description: 'Find and Evaluate trade goods in Avorion', icon: '📈'},
@@ -11,10 +13,8 @@ const pages = [
 
 const Index = () => (
   <Layout title='Home'>
-    <BigTitle height={60}>
-      Welcome to<br/>
-      Henrixounez&apos;s<br/>
-      Website
+    <BigTitle height={30}>
+      Henrixounez
     </BigTitle>
     <LinkList list={pages}/>
   </Layout>
